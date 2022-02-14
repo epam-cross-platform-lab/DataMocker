1ba1bf00afacd6e40e3e76ffa745af35c7b3783a
Denis Zhukovski <dzhukovsky@softek.by>
2022-02-14 20:45:47 +0300

    fix unit tests

--- tests/DataMocker.Tests.UnitTests/EmbeddedResourceHttpHandlerTests.cs
+++ tests/DataMocker.Tests.UnitTests/EmbeddedResourceHttpHandlerTests.cs
@@ -9,12 +9,11 @@ namespace DataMocker.Tests.UnitTests
     [TestClass]
     public class EmbeddedResourceHttpHandlerTests
     {
-        private string _resourceUri = "http://wdc-logcnt.eurocenter.be:80/webservice/apiscanning.php?filter=getListCmr&date=2022-02-11";
+        private string _resourceUri = "http://test.server.be:80/webservice/apiscanning.php?filter=getData&date=2022-02-11";
 
         [TestMethod]
         public async Task ResourceNotFound()
         {
-            Routes.AddRoute("webservice/apiscanning.php?{id}");
             var response = await new HttpClient(
                 new MockHandlerInitializer(
                     "",
@@ -28,7 +27,7 @@ namespace DataMocker.Tests.UnitTests
             );
 
             Assert.AreEqual(
-                 "Resource file hasn't been found: [get_webservice_apiscanning_php.json, get_webservice_apiscanning_php(191EC4BC).json]",
+                 "Resource file hasn't been found: [get_webservice_apiscanning_php.json, get_webservice_apiscanning_php(6E4E394A).json]",
                  await response.Content.ReadAsStringAsync()
             );
         }
