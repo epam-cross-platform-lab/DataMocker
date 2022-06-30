7c41cd9c287d446049eef60f8bbd58ea78164447
AndreiMelnikau <46218971+AndreiMelnikau@users.noreply.github.com>
2022-06-30 13:11:02 +0300

    improve logging; removing silent configuration from uitests (#8)

---
+++ src/DataMocker.Mock/MockLogger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace DataMocker.Mock
+{
+    public static class MockLogger
+    {
+        private static Action<string> logMethod;
+
+        public static void SetLogMethod(Action<string> action)
+        {
+            logMethod = action;
+        }
+
+        internal static void Log(string message)
+        {
+            if (logMethod != null)
+            {
+                logMethod.Invoke(message);
+            }
+        }
+    }
+}
